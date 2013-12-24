5281cdc531ce166aefc58b394ab3fea80c74398c
Sumant Chhunchha <mr.chhunchha@gmail.com>
2013-12-24 15:20:40 -0800

    update application layout

--- learn-rails/app/controllers/visitors_controller.rb
+++ learn-rails/app/controllers/visitors_controller.rb
@@ -4,5 +4,11 @@ class VisitorsController < ApplicationController
 		@owner = Owner.new
 		#explicit code for rendering, otherwsie as new method will use new.html.erb
 		#render 'visitors/new'
+		flash[:notice] = 'Welcome!'
+		if @owner.countdown < 50
+			flash.now[:alert] = 'My birthday is soon.'
+		else
+			flash.now[:alert] = 'My birthday is far far away.'
+		end
 	end
 end
