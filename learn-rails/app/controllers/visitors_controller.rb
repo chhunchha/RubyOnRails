63fe1d9fdad3f93aeb7d425986dbcf8fcde0086e
Sumant Chhunchha <mr.chhunchha@gmail.com>
2013-12-24 15:38:05 -0800

    add 'about' and 'contact' pages and links

--- learn-rails/app/controllers/visitors_controller.rb
+++ learn-rails/app/controllers/visitors_controller.rb
@@ -4,7 +4,6 @@ class VisitorsController < ApplicationController
 		@owner = Owner.new
 		#explicit code for rendering, otherwsie as new method will use new.html.erb
 		#render 'visitors/new'
-		flash[:notice] = 'Welcome!'
 		if @owner.countdown < 50
 			flash.now[:alert] = 'My birthday is soon.'
 		else
