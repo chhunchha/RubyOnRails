b48cf7f14289e9a9f2c931e9f80ed50ab4b0f973
Sumant Chhunchha <mr.chhunchha@gmail.com>
2013-12-25 23:42:24 -0800

    sending mail

--- learn-rails/app/controllers/contacts_controller.rb
+++ learn-rails/app/controllers/contacts_controller.rb
@@ -7,8 +7,9 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(secure_params)
 		if @contact.valid?
-			# TODOD save data
+	
 			@contact.update_spreadsheet
+			UserMailer.contact_email(@contact).deliver
 			flash[:notice] = "Message sent from #{@contact.name}."
 			redirect_to root_path
 		else
